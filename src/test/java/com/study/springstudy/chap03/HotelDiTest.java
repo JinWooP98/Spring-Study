aa9b7b97b02bc0fc4eeb61fb3a7fc607ab384611
JinWooP98 <qwdk0406@naver.com>
2024-05-13 17:02:33 +0900

    # initial commit & SOLID 개념 학습

---
+++ src/test/java/com/study/springstudy/chap03/HotelDiTest.java
@@ -0,0 +1,19 @@
+package com.study.springstudy.chap03;
+
+import com.study.springstudy.chap03.config.HotelManager;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HotelDiTest {
+
+    @Test
+    void diTest () {
+        HotelManager hotelManager = new HotelManager();
+
+        Hotel hotel = hotelManager.hotel2();
+        hotel.inform();
+
+    }
+
+}
