570051e6d454cb560a954a861a520e3622661925
JinWooP98 <qwdk0406@naver.com>
2024-06-03 11:34:53 +0900

    # 자동로그인 처리 (interceptor)

--- src/main/java/com/study/springstudy/springmvc/chap05/service/MemberService.java
+++ src/main/java/com/study/springstudy/springmvc/chap05/service/MemberService.java
@@ -88,7 +88,13 @@ public class MemberService {
 
         }
 
+        maintainLoginState(session, foundMember);
 
+
+        return SUCCESS;
+    }
+
+    public static void maintainLoginState(HttpSession session, Member foundMember) {
         log.info("{}님 로그인 성공", foundMember.getName());
 
         // 세션의 수명 : 설정된 시간 OR 브라우저를 닫기 전까지
@@ -99,7 +105,6 @@ public class MemberService {
         log.debug("session time: {}", maxInactiveInterval);
 
         session.setAttribute("login", new LoginUserInfoDto(foundMember));
-        return SUCCESS;
     }
 
     // 아이디, 이메일 중복검사
